d56b67ca568695c476692ce745d4454079809778
APIVerve Team <hello@apiverve.com>
2025-03-28 17:58:09 -0500

    Bug Fixes

--- src/apiverve_thesaurus/apiClient.py
+++ src/apiverve_thesaurus/apiClient.py
@@ -10,10 +10,10 @@ class ThesaurusAPIClient:
         
         self.api_key = api_key
         self.secure = secure
-        self.base_url = 'https://api.apiverve.com/v1.0.9/thesaurus'
+        self.base_url = 'https://api.apiverve.com/v1/thesaurus'
         self.headers = {
             'x-api-key': self.api_key,
-            'auth-mode': 'pypi'
+            'auth-mode': 'pypi-package'
         }
 
     def execute(self, params=None):
